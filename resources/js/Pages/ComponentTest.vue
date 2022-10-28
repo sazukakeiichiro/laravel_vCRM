a2c21c626b8e15298d2b183bb7738d0fcc205ec7
sazukakeiichiro <sdk.kicr7@icloud.com>
2022-10-28 11:36:44 +0900

    準備

---
+++ resources/js/Pages/ComponentTest.vue
@@ -0,0 +1,11 @@
+<script setup>
+import GuestLayout from '@/Layouts/Guest.vue'
+import Label from '@/Components/Label.vue'
+import Input from '@/Components/Input.vue'
+</script>
+
+<template>
+<GuestLayout>
+適当に
+</GuestLayout>
+</template>
